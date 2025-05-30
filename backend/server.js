52e51bc24118df0f51ed00c685386ec19d948c7c
Souvik-31 <souvik.sarkar3103@gmail.com>
2025-05-30 14:08:30 +0530

    commit1

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import productsRoutes from "./routes/products.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());//allow express to parse json data
+app.use(cookieParser());//allow express to parse cookies
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:"+ PORT);
+
+    connectDB();
+});
+
+
+// 6HJkeGc3bpHleBJf
---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { ShoppingCart } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
+
+const ProductCard = ({ product }) => {
+	const { user } = useUserStore();
+	const { addToCart } = useCartStore();
+	const handleAddToCart = () => {
+		if (!user) {
+			toast.error("Please login to add products to cart", { id: "login" });
+			return;
+		} else {
+			// add to cart
+			addToCart(product);
+		}
+	};
+
+	return (
+		<div className="flex w-full flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg">
+  <div>
+    <img
+      className="w-full h-48 object-cover"
+      src={product.image}
+      alt={product.name}
+    />
+  </div>
+
+  <div className="flex flex-col p-5">
+    <h5 className="text-xl font-semibold tracking-tight text-white">{product.name}</h5>
+    
+    <div className="mt-2 mb-5">
+      <span className="text-3xl font-bold text-emerald-400">₹{product.price}</span>
+    </div>
+
+    <button
+      className="mt-auto flex items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300"
+      onClick={handleAddToCart}
+    >
+      <ShoppingCart size={22} className="mr-2" />
+      Add to cart
+    </button>
+  </div>
+</div>
+
+	);
+};
+export default ProductCard;
